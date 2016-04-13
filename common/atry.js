d980a96866657232325e89e947df358a5b696f78
978548481@qq.com <d7577355>
2016-04-13 13:34:01 +0800

    图片

---
+++ common/atry.js
@@ -0,0 +1,32 @@
+var querystring = require('querystring');
+var util = require('util');
+var mysql = require('mysql');
+var http = require('http');
+var fs = require('fs');
+var iconv = require('iconv-lite')
+var bodyParser = require('body-parser');
+var config = require('../config.js');
+var conn = mysql.createConnection(config.database_info);
+conn.connect();
+
+
+exports.try = function (req, res, next) {
+    var file = config.upload.path_uploadpage + "a.json";
+    fs.readFile(file, function (err, data) {
+        if (err) {
+            // console.log("读取文件fail " + err);
+            res.json("读取文件fail " + err);
+        }else
+        {
+            // 读取成功时
+            // 输出字节数组
+            console.log(data);
+            // 把数组转换为gbk中文
+            var str = iconv.decode(data, 'gbk');
+            // console.log(str);
+            res.json(JSON.parse(str));
+        }
+    });
+
+};
+
--- routes/com.js
+++ routes/com.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var upload = require('../common/upload');
+var atry = require('../common/atry');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('通用接口');
+router.get('/', function (req, res, next) {
+    res.send('通用接口');
 });
 router.post('/upload', upload.uploadfile);
+router.get('/try', atry.try);
 
 module.exports = router;
